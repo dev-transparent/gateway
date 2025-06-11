9dddebca26e4629b86b8a6a25f3f56613ed357af
Gary Mardell <info@garymardell.co.uk>
2025-06-11 16:24:55 -0700

    Export traces to local http endpoint

--- config/telemetry.cr
+++ config/telemetry.cr
@@ -1,5 +1,13 @@
 OpenTelemetry.configure do |config|
   config.service_name = "transparent-gateway"
   config.service_version = "0.0.1"
-  config.exporter = OpenTelemetry::Exporter.new(variant: :stdout)
+
+  if LuckyEnv.development?
+    config.exporter = OpenTelemetry::Exporter.new(variant: :http) do |exporter|
+      exporter = exporter.as(OpenTelemetry::Exporter::Http)
+      exporter.endpoint = "http://localhost:3000/metrics"
+    end
+  else
+    config.exporter = OpenTelemetry::Exporter.new(variant: :stdout)
+  end
 end
