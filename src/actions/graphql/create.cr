511e42dcae94880808282dadcb4781466afb8de8
Gary Mardell <info@garymardell.co.uk>
2025-06-11 14:59:28 -0700

    Add query, operation name and upstream response to spans

--- src/actions/graphql/create.cr
+++ src/actions/graphql/create.cr
@@ -7,14 +7,26 @@ class Graphql::Create < ApiAction
       query = Oxide::Query.from_json(params.body)
       query.document(max_tokens: config.parsing.try &.max_tokens)
 
+      # Add query information to the trace
+      if operation_name = query.operation_name
+        span["transparent.operation_name"] = operation_name
+      end
+
+      span["transparent.query"] = query.query_string
+      span["transparent.post"] = "POST"
+
+
       # Forward the query to upstream
       client = HTTP::Client.new(config.upstream)
 
-      request = OpenTelemetry.tracer.in_span("transparent.upstream") do
+      request = OpenTelemetry.tracer.in_span("transparent.upstream") do |span|
         client.post(
           path: config.upstream.path,
           body: params.body
-        )
+        ).tap do |request|
+          span["transparent.status_code"] = request.status_code
+          span["transparent.response_size"] = request.body.bytesize
+        end
       end
 
       # Copy over the headers
