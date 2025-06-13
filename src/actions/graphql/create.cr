42d3e91a05ca161cb4bac141e62309acb192ab5c
Gary Mardell <info@garymardell.co.uk>
2025-06-12 17:50:00 -0700

    Propagate the opentelemetry headers to upstream

--- src/actions/graphql/create.cr
+++ src/actions/graphql/create.cr
@@ -1,29 +1,32 @@
 class Graphql::Create < ApiAction
   post "/graphql" do
-    OpenTelemetry.tracer.in_span("transparent.graphql") do |span|
+    OpenTelemetry.tracer.in_span("transparent.request") do |span|
       config = Gateway::ConfigLoader.for_request(request)
 
       # Parse the query to generate the AST
       query = Oxide::Query.from_json(params.body)
       query.document(max_tokens: config.parsing.try &.max_tokens)
 
-      # Add query information to the trace
-      if operation_name = query.operation_name
-        span["transparent.operation_name"] = operation_name
-      end
-
-      span["transparent.query"] = query.query_string
-      span["transparent.post"] = "POST"
-
-
       # Forward the query to upstream
       client = HTTP::Client.new(config.upstream)
 
-      request = OpenTelemetry.tracer.in_span("transparent.upstream") do |span|
+      request = OpenTelemetry.tracer.in_span("transparent.operation") do |span|
+        headers = HTTP::Headers.new
+
+        OpenTelemetry::Propagation::TraceContext.new(span.context).inject(headers)
+
         client.post(
           path: config.upstream.path,
-          body: params.body
+          body: params.body,
+          headers: headers
         ).tap do |request|
+          # Add query information to the trace
+          if operation_name = query.operation_name
+            span["transparent.operation_name"] = operation_name
+          end
+
+          span["transparent.query"] = query.query_string
+          span["transparent.method"] = "POST"
           span["transparent.status_code"] = request.status_code
           span["transparent.response_size"] = request.body.bytesize
         end
