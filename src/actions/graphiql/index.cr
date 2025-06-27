4a0ba36026dae5aaba2db8b7f588cd06465bc56f
Gary Mardell <info@garymardell.co.uk>
2025-06-26 17:04:45 -0700

    Skip the graphiql interface in production

--- src/actions/graphiql/index.cr
+++ src/actions/graphiql/index.cr
@@ -1,7 +1,17 @@
 class Graphiql::Index < ApiAction
+  before ensure_not_production
+
   get "/graphiql" do
     data {{ read_file("#{__DIR__}/index.html") }},
       disposition: "inline",
       content_type: "text/html"
+  end
+
+  private def ensure_not_production
+    if LuckyEnv.production?
+      raise Lucky::RouteNotFoundError.new(context)
+    else
+      continue
+    end
   end
 end
